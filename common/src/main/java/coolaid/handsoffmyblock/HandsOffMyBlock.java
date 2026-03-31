a1dee3c022bee53ef380867fd3d94fe48134840b
Aidan <96690746+CoolAid48@users.noreply.github.com>
2026-03-31 08:46:33 -0400

    Rename MOD_ID and update initialization logs
    early development on mod rebrand

--- common/src/main/java/coolaid/handsoffmyblock/HandsOffMyBlock.java
+++ common/src/main/java/coolaid/handsoffmyblock/HandsOffMyBlock.java
@@ -5,13 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HandsOffMyBlock {
-    public static final String MOD_ID = "handsoffmyblock";
+    public static final String MOD_ID = "village-pillage-tweaks";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
         HandsOffMyConfigManager.load();
 
         LOGGER.info("Getting marked POI data!");
-        LOGGER.info("Hands off! Initializing HOMB");
+        LOGGER.info("Calculating Raid Radius!");
+        LOGGER.info("Initializing tweaks...");
     }
 }
